2178b9712892711714c9892bf26dd4d31062dbcf
Angelitorl <an1997991@gmail.com>
2018-12-20 08:36:13 +0100

    Basic movement
    Cambiar de carril pendiente

--- RoadsOfSalvation/Assets/_Scripts/PlayerController.cs
+++ RoadsOfSalvation/Assets/_Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    private float acel = 1000.0f;
-    private float velBase;
-    private float velMax;
-    
+    public float acel = 100.0f; //Aceleracion
+    public float velMin = 10.0f; //Velocidad maxima
+    public float velMax = 30.0f; //Velocidad minima
+
+    public string upKey = "w";
+    public string downKey = "s";
 
     private Rigidbody rb;
 
@@ -21,12 +23,42 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        if (moveHorizontal > velMax) moveHorizontal = velMax;
+        float moveHorizontal = Input.GetAxis("Horizontal"); // Valor entre 1 y -1.
 
-        movement = new Vector3(moveHorizontal+ velBase, 0.0f, 0.0f);
+        //Si la velocidad ya es maxima, ignora el input positivo
+        if(moveHorizontal>0 && rb.velocity.x >= velMax)
+        {
+            rb.velocity = new Vector3(velMax, rb.velocity.y, 0.0f);
+        }
+        //Si la velocidad ya es minima, ignora el input negativo. 
+        //Ademas, si el objeto no llega a la velocidad minima, se le da (solo ocurre una vez) 
+        else if (moveHorizontal <= 0 && rb.velocity.x <= velMin)
+        {
+            rb.velocity = new Vector3(velMin, rb.velocity.y, 0.0f);
+        }
+        //Caso normal en el que la aceleracion/deceleracion depende del input
+        else if (moveHorizontal != 0)
+        {
+            movement = new Vector3(moveHorizontal * acel, 0.0f, 0.0f);
+            rb.AddForce(movement);
+        }
+        
+    }
 
-        rb.AddForce(movement * acel);
+    private void Update()
+    {
+        if (Input.GetKeyDown(upKey))
+        {
+            print("Parriba");
+            //temporal y no funsiona
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - GetComponent<Collider>().bounds.size.z);
+        }
+        else if (Input.GetKeyDown(downKey))
+        {
+            print("Pabajo");
+            //temporal y no funsiona
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + GetComponent<Collider>().bounds.size.z);
+        }
     }
 }
 
