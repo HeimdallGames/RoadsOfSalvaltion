680a4e8116395aa74f9aa51937440394315878ea
cruzIago <iagocruzgarcia@gmail.com>
2019-01-04 11:12:10 +0100

    Cambio en el movimiento
    Se mueve más rapido y fluido.
    Todavía hay conflictos con las físicas

--- RoadsOfSalvation/Assets/_Scripts/PlayerController.cs
+++ RoadsOfSalvation/Assets/_Scripts/PlayerController.cs
@@ -6,12 +6,13 @@ public class PlayerController : MonoBehaviour
 {
 
     public float acel = 100.0f; //Aceleracion
-    public float velMin = 10.0f; //Velocidad maxima
-    public float velMax = 30.0f; //Velocidad minima
+    public float velMin = 300.0f; //Velocidad maxima
+    public float velMax = 600.0f; //Velocidad minima
 
     public string upKey = "w";
     public string downKey = "s";
 
+    private int posHorizontal = 0; // El carril donde se encuentra el vehiculo
     private Rigidbody rb;
 
     Vector3 movement;
@@ -21,45 +22,42 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+
     void FixedUpdate()
     {
+
         float moveHorizontal = Input.GetAxis("Horizontal"); // Valor entre 1 y -1.
 
+
         //Si la velocidad ya es maxima, ignora el input positivo
-        if(moveHorizontal>0 && rb.velocity.x >= velMax)
+        if (moveHorizontal > 0 && rb.velocity.x <= velMax)
         {
             rb.velocity = new Vector3(velMax, rb.velocity.y, 0.0f);
         }
         //Si la velocidad ya es minima, ignora el input negativo. 
         //Ademas, si el objeto no llega a la velocidad minima, se le da (solo ocurre una vez) 
-        else if (moveHorizontal <= 0 && rb.velocity.x <= velMin)
+        else if (moveHorizontal < 0 || rb.velocity.x <= velMin)
         {
             rb.velocity = new Vector3(velMin, rb.velocity.y, 0.0f);
         }
-        //Caso normal en el que la aceleracion/deceleracion depende del input
-        else if (moveHorizontal != 0)
-        {
-            movement = new Vector3(moveHorizontal * acel, 0.0f, 0.0f);
-            rb.AddForce(movement);
-        }
-        
-    }
 
+
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(upKey))
+        if (Input.GetKeyDown(upKey) && posHorizontal < 1)
         {
             print("Parriba");
-            //temporal y no funsiona
-            if(transform.position.x < 0)
-                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 2.5f);
+            posHorizontal += 1;
+            rb.MovePosition(transform.position + (Vector3.forward * 3));
         }
-        else if (Input.GetKeyDown(downKey))
+        else if (Input.GetKeyDown(downKey) && posHorizontal > -1)
         {
             print("Pabajo");
-            //temporal y no funsiona
-            if (transform.position.x < -5)
-                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 2.5f);
+            posHorizontal -= 1;
+            rb.MovePosition(transform.position + (Vector3.back * 3));
+
+
         }
     }
 }
