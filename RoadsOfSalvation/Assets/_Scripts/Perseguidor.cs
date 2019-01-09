6288fe29c505c588087116eb89e2af489980b04c
Angelitorl <an1997991@gmail.com>
2019-01-09 12:50:25 +0100

    Primera aproximacion de perseguidores

---
+++ RoadsOfSalvation/Assets/_Scripts/Perseguidor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perseguidor : MonoBehaviour {
+
+    public GameObject objectoLanzado;
+    public float frecuenciaLanzamiento = 2;
+    public float inicioLanzamientos = 3;
+
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Lanzamiento", inicioLanzamientos, frecuenciaLanzamiento);
+	}
+	
+    void Lanzamiento()
+    {
+        GameObject obj = Instantiate(objectoLanzado, transform.position, transform.rotation);
+        obj.tag = "finish";
+        obj.AddComponent<moveForwardConstantly>();
+    }
+        
+}
---
+++ RoadsOfSalvation/Assets/_Scripts/moveForwardConstantly.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveForwardConstantly : MonoBehaviour {
+	
+    // Update is called once per frame
+    void FixedUpdate () {
+        transform.position = new Vector3(transform.position.x+1, transform.position.y, transform.position.z);
+        //Destruir objeto si X distancia más del personaje
+    }
+}
