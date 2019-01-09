4669b1bb6d61b2a735f7de522ead1dd5d59c7503
Angelitorl <an1997991@gmail.com>
2019-01-09 18:10:32 +0100

    Actualizados prefabs de coleccionables

--- RoadsOfSalvation/Assets/_Scripts/RotatingObj.cs
+++ RoadsOfSalvation/Assets/_Scripts/RotatingObj.cs
@@ -6,6 +6,6 @@ public class RotatingObj : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0, 0, 50 * Time.deltaTime);
+        //transform.Rotate(0, 0, 50 * Time.deltaTime);
     }
 }
