75a36e7e40dfa27596576905dc18f1972a8e71c3
cruzIago <iagocruzgarcia@gmail.com>
2019-01-09 19:41:43 +0100

    Escenario bueno2 y malo1
    Ancianas completas
    Correccion de algunas colisiones

---
+++ RoadsOfSalvation/Assets/_Scripts/ancianaGodPath_moves.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ancianaGodPath_moves : MonoBehaviour {
+    private Vector3 start;
+    private Vector3 nueva;
+
+    private float vel = 0.5f;
+    private float maxDist = 0.007f;
+
+    void Start () {
+        start = transform.localPosition;
+        nueva = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (start.y == 0)
+        {
+            nueva.y = start.y + (maxDist * Mathf.Sin(Time.time * vel));
+            transform.localPosition = nueva;
+        }
+        else if (start.y == maxDist)
+        {
+           
+            nueva.y = start.y - ((maxDist*2) * Mathf.Abs(Mathf.Sin(Time.time * vel)));
+            transform.localPosition = nueva;
+
+        }
+        else if (start.y == -maxDist) {
+            
+            nueva.y = start.y + ((maxDist*2) * Mathf.Abs(Mathf.Sin(Time.time * vel)));
+            transform.localPosition = nueva;
+        }
+	}
+}
---
+++ RoadsOfSalvation/Assets/_Scripts/ancianaSatanPath_moves.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ancianaSatanPath_moves : MonoBehaviour {
+
+    private Vector3 start;
+    private Vector3 nueva;
+
+    private float vel = 0.5f;
+    private float maxDist = 0.05f;
+
+    void Start()
+    {
+        start = transform.localPosition;
+        nueva = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (start.y == -0.025f)
+        {
+            Debug.Log("1");
+            nueva.y = start.y + ((maxDist/2) * Mathf.Sin(Time.time * vel*2));
+            transform.localPosition = nueva;
+        }
+        else if (start.y == 0)
+        {
+
+            Debug.Log("2");
+            nueva.y = start.y - (maxDist  * Mathf.Abs(Mathf.Sin(Time.time * vel)));
+            transform.localPosition = nueva;
+
+        }
+        else if (start.y == -maxDist)
+        {
+
+            Debug.Log("3");
+            nueva.y = start.y + (maxDist * Mathf.Abs(Mathf.Sin(Time.time * vel)));
+            transform.localPosition = nueva;
+        }
+    }
+}
+
