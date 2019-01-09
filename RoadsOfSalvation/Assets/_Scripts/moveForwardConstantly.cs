b759b02409bff8624fdfd979c1ffbadca9d717fa
Angelitorl <an1997991@gmail.com>
2019-01-09 14:32:09 +0100

    See description
    Materiales del cura retocados
    Cura ya no choca
    proyectiles van por fuerza, pero no van
    eliminado segundo script playerController del player malvado

--- RoadsOfSalvation/Assets/_Scripts/moveForwardConstantly.cs
+++ RoadsOfSalvation/Assets/_Scripts/moveForwardConstantly.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class moveForwardConstantly : MonoBehaviour {
-	
+    
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(new Vector3(15f, 0, 0), ForceMode.VelocityChange);
+    }
+
     // Update is called once per frame
     void FixedUpdate () {
-        transform.position = new Vector3(transform.position.x+1, transform.position.y, transform.position.z);
+        //Debug.Log(rb.velocity);
         //Destruir objeto si X distancia más del personaje
     }
 }
